b7388408fc642884f1fe5f9f8467e57857014a9b
Haider ali <75686258+Haider-Ali-Dev@users.noreply.github.com>
2021-09-28 21:46:48 +0400

    files

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Pokemon from './Pokemon';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, Image, TouchableOpacity, Pressable } from 'react-native';
 
 export default function App() {
+
+  const [inputPokimon, setPokimonInput] = useState("")
+  const [pokemon, setPokemon] = useState(null)
+  const [location, setLocation] = useState(null)
+  const [item, setItem] = useState(null)
+  const onPokimonPress = async () => {
+    if (inputPokimon) {//
+        try {
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputPokimon}/`)
+        const jsonData = await data.json()
+        setPokemon(jsonData)
+      } catch (err) {
+        return 
+      }
+      try {
+        const locationData = await fetch(`https://pokeapi.co/api/v2/location/${inputPokimon}/`)
+        const jsonLocation = await locationData.json()
+        setLocation(jsonLocation)
+      } catch(err) {
+        return
+      }
+
+      try {
+        const itemData = await fetch(`https://pokeapi.co/api/v2/item/${inputPokimon}/`)
+        const itemJson = await itemData.json()
+        setItem(itemJson)
+      } catch {
+        return
+      }
+    } 
+    
+    
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image} source={require('./poki.png')}/>
+      <SafeAreaView>
+        <TextInput style={styles.input} placeholder={"Enter a Pokemon Name or ID"} value={inputPokimon} onChangeText={setPokimonInput}></TextInput>
+        {pokemon ?<Pokemon pokemon={pokemon} location={location} item={item}/>: <Text style={styles.stats}>No Stats</Text>}
+        <View style={styles.buttonFlex}>
+          <TouchableOpacity style={styles.button} onPress={onPokimonPress}>
+            <Text style={styles.white}>Generate Stats</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.clear} onPress={() => setPokemon(null)}>
+            <Text style={styles.white}>Clear</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#ffff00",
+    margin: 0,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  input: {
+    borderWidth: 2,
+    borderColor: 'black',
+    width: 300,
+    borderRadius: 20,
+    padding: 5,
+    marginRight: 30,
+    marginLeft: 10
+  },
+  image: {
+    margin: 20,
+    width: 200,
+    height: 100
+  },
+  button: {
+    flexShrink: 1,
+    borderWidth: 2,
+    borderColor: "black",
+    padding: 5,
+    color: "white",
+    marginBottom: 10,
+    backgroundColor: 'blue',
+    width: 120,
+    borderRadius: 10,
+    height: 30,
+  },
+  clear: {
+    fontSize: 1000,
+    width: 50,
+    borderWidth: 2,
+    borderColor: "black",
+    height: 30,
+    color: "white",
+    backgroundColor: 'blue',
+    padding: 5,
+    borderRadius: 10,
+    marginRight: 50,
+  },
+  buttonFlex: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
+    position: 'relative',
+    
+  },
+  stats: {
+    width: 300,
+    margin:10,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#ffff00",
+    padding: 20,
+    backgroundColor: "#ADD8E6",
+    textAlign: "center",
+},
+white: {
+  color: "white",
+  padding: 1,
+  fontWeight: "600"
+}
+
 });
---
+++ Pokemon.js
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, Image, } from 'react-native';
+import React from 'react'
+
+const Pokemon = ({ pokemon, location, item }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.main}>Information</Text>
+            <Text style={styles.text}>Name: {pokemon.name.toUpperCase()}</Text>
+            <Text style={styles.text}>ID: {pokemon.id}</Text>
+            <Text  style={styles.text}>Base Experince: {pokemon.base_experience}</Text>
+            <Text  style={styles.text}>Height: {pokemon.height}</Text>
+            <Text style={styles.main}>Abilities</Text>
+            <Text style={styles.text}>Abilitiy: {pokemon ? pokemon.abilities[0].ability.name : null}</Text>
+
+            <Text style={styles.main}>Stats</Text>
+            <Text style={styles.text}>Base Stat: {pokemon.stats[0].base_stat}</Text>
+            <Text style={styles.text}>Effort: {pokemon.stats[0].effort}</Text>
+            <Text style={styles.text}>Name: {pokemon.stats[0].stat.name}</Text> 
+            <Text style={styles.main}>Types</Text>
+            <Text style={styles.text}>Type: {pokemon.types[0].type.name}</Text>
+
+            <Text style={styles.main}>Location</Text>
+            <Text style={styles.text}>Location Name: {location ? location.name : "None"}</Text>
+            <Text style={styles.text}>Region: {location ? location.region.name: "None"}</Text>
+
+            <Text style={styles.main}>Items</Text>
+            <Text style={styles.text}>Item Name: {item ? item.name : null}</Text>
+            <Text style={styles.text}>Fling Power: {item ? item.fling_power : <Text>None</Text>}</Text>
+            <Text style={styles.text}>Item Cost: {item ? item.cost : null}</Text>
+            <Text style={styles.text}>Effect: {item ? item.effect_entries[0].effect : null}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 300,
+        margin:10,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: "#ffff00",
+        padding: 20,
+        backgroundColor: "#ADD8E6",
+    },
+    image: {
+        width: 100,
+        height: 100
+    },
+    text:{
+        fontSize: 17,
+        fontWeight: "800",//
+    },
+    main: {
+        textAlign: "center",
+        fontSize: 22,
+        marginBottom: 10,
+        textDecorationLine: "underline"
+    }
+})
+
+export default Pokemon
